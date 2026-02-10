3df2ae9dfa2d5e843e9a8f57c5ef72b7d4258d00
Lavinsh Parsai <lavinsh.parsai08@gmail.com>
2026-02-10 15:03:10 -0500

    pushing the chnages from 2025

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
     plugins: [
         react(),
         keycloakify({
-            accountThemeImplementation: "Single-Page"
+            accountThemeImplementation: "Single-Page",
+            extraThemeProperties: ["darkMode=false"]
         })
     ]
 });
